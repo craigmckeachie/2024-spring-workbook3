a66294106268a7d9429f72dc562344d0c5fb5641
Craig McKeachie <craigdmckeachie@gmail.com>
2024-04-30 10:46:10 -0400

    move bake to own file

--- demos/add.js
+++ demos/add.js
@@ -15,10 +15,4 @@ add(3,3)
 
 
 
-// function bake(ingredient1, ingredient2, ingredient3) {
-//     if(ingredient1 =="apples"){
-//         console.log("apple pie");
-//     }
-// }
 
-// bake("apples", "cinnamon", "sugar")
