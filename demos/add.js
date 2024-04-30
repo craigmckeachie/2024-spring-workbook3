201310508154eb138425afc55d349889fb56f23a
Craig McKeachie <craigdmckeachie@gmail.com>
2024-04-30 08:49:09 -0400

    add updates

--- demos/add.js
+++ demos/add.js
@@ -1,3 +1,8 @@
+//number 1 and number 2 are function parameters
+function log(value) {
+    
+}
+
 function add(number1, number2) {
   var result = number1 + number2;
   console.log(result);
@@ -5,5 +10,19 @@ function add(number1, number2) {
 
 var x = 67;
 var y = 34;
-
+//x and y are arguments being passed to the function
 add(x, y);
+
+add(1,1)
+add(2,2)
+add(3,3)
+
+
+
+// function bake(ingredient1, ingredient2, ingredient3) {
+//     if(ingredient1 =="apples"){
+//         console.log("apple pie");
+//     }
+// }
+
+// bake("apples", "cinnamon", "sugar")
