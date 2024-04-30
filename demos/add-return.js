ab69cbe6b38af7587fad1d6d52cfa69e014f62b1
Craig McKeachie <craigdmckeachie@gmail.com>
2024-04-30 10:56:49 -0400

    add return

---
+++ demos/add-return.js
@@ -0,0 +1,12 @@
+function add(number1, number2) {
+  var result = number1 + number2;
+  return result;
+}
+
+//a
+var resultCaught = add(2, 2);
+console.log(resultCaught);
+//OR
+
+//b
+console.log(add(2, 2));
