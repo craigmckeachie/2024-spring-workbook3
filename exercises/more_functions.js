4e70e512a2f69ccf49f1c6d74a7dcad2c3d4888c
Craig McKeachie <craigdmckeachie@gmail.com>
2024-04-30 10:05:35 -0400

    display receipt part 2

--- exercises/more_functions.js
+++ exercises/more_functions.js
@@ -4,11 +4,18 @@ function displayReceipt(totalDue, amountPaid) {
   var change = amountPaid - totalDue;
   console.log("Total Due: $" + totalDue);
   console.log("Amount Paid: $" + amountPaid);
-  console.log("Change: $" + change);
+
+  if (change > 0) {
+    console.log("Change: $" + change);
+  } else if (change == 0) {
+    console.log("No change needed");
+  } else if (change < 0) {
+    console.log("You still owe another: $" + Math.abs(change));
+  }
+
   console.log();
 }
 
 displayReceipt(11.75, 20);
 displayReceipt(11.75, 11.75);
 displayReceipt(11.75, 10);
-
