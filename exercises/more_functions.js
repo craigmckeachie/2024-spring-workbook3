dcd776d395252e6850f3ad9739f616189ad173ad
Craig McKeachie <craigdmckeachie@gmail.com>
2024-04-30 10:12:26 -0400

    display receipt broken down more

--- exercises/more_functions.js
+++ exercises/more_functions.js
@@ -6,11 +6,13 @@ function displayReceipt(totalDue, amountPaid) {
   console.log("Amount Paid: $" + amountPaid);
 
   if (change > 0) {
-    console.log("Change: $" + change);
+    console.log("Change: $" + change.toFixed(2));
   } else if (change == 0) {
     console.log("No change needed");
   } else if (change < 0) {
-    console.log("You still owe another: $" + Math.abs(change));
+    change = Math.abs(change);
+    var changeFormatted = change.toFixed(2);
+    console.log("You still owe another: $" + changeFormatted);
   }
 
   console.log();
@@ -19,3 +21,4 @@ function displayReceipt(totalDue, amountPaid) {
 displayReceipt(11.75, 20);
 displayReceipt(11.75, 11.75);
 displayReceipt(11.75, 10);
+displayReceipt(22.65, 15);
