b6f6403cc412026c49325541f6aec49f28cc16cd
Craig McKeachie <craigdmckeachie@gmail.com>
2024-04-30 13:23:24 -0400

    youtube

---
+++ demos/you-tube.js
@@ -0,0 +1,23 @@
+//common use case
+function loadVideos(pageNumber, numberOfVideos) {
+  //call api and get data
+  return videos;
+}
+
+function displayVideos(videos) {
+  //get html elements on the page and fill in data
+  //div element innerText = video info
+}
+
+function loadShorts() {
+  //call api and get data
+  return shorts;
+}
+
+function displayShorts(shorts) {
+  //get html elements on the page and fill in data
+  //div element innerText = short info
+}
+
+//loadSubscriptions, displaySubscriptions
+//loadTags, displayTags
