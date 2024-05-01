ef1b11e0207b9e449cb80d5fae5224b723416013
Craig McKeachie <craigdmckeachie@gmail.com>
2024-05-01 10:36:21 -0400

    string search

--- demos/string-search.js
+++ demos/string-search.js
@@ -1,3 +1,4 @@
-let name = "Betty Jo Smalltree";
-let pos = name.indexOf(" ");
+let fullName = "Mark Westley";
+let pos = fullName.indexOf(" ");
+console.log(pos);
 // finds the position of the first space (ex: 5)
