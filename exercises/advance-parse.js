5f5e70c882b1698fb7ee3bd7b67b132d9bc4a59f
Craig McKeachie <craigdmckeachie@gmail.com>
2024-05-01 20:24:13 -0400

    advance part v2

--- exercises/advance-parse.js
+++ exercises/advance-parse.js
@@ -1,25 +1,25 @@
-
-function parseAndDisplayName(name) {
-  if (typeof name === "string") {
-    let nameSpace = name.split(" ");
-
-    let firstName = nameSpace[0];
-    let middleName = "";
-    let lastName = "";
-
-    if (nameSpace.length > 2) {
-      middleName = nameSpace[1];
-      lastName = nameSpace.slice(2).join(" ");
-    } else if (nameSpace.length === 2) {
-      lastName = nameSpace[1];
-    }
-    console.log(`Name: ${name}`);
-    console.log(`First name: ${firstName}`);
-    console.log(`Middle name: ${middleName}`);
-    console.log(`Last name: ${lastName}`);
-  } else {
+function parseAndDisplayName(fullName) {
+  if (fullName && typeof fullName != "string") {
     console.log("Error: The name must be a string");
   }
+
+  let names = fullName.split(" ");
+
+  let firstName = names[0];
+  let middleName = "";
+  let lastName = "";
+
+  if (names.length == 2) {
+    lastName = names[1];
+  } else if (names.length == 3) {
+    middleName = names[1];
+    lastName = names[2];
+  }
+
+  console.log(`Name: ${fullName}`);
+  console.log(`First name: ${firstName}`);
+  console.log(`Middle name: ${middleName}`);
+  console.log(`Last name: ${lastName}`);
 }
 parseAndDisplayName("Cher");
 parseAndDisplayName("Brenda Kaye");
