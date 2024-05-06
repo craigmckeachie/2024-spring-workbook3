e81d7bc4d0d1dee18b3daa0be3b9bdbc7b48a660
Craig McKeachie <craigdmckeachie@gmail.com>
2024-05-06 14:44:21 -0400

    ice cream part 2

--- exercises/ice-cream/scripts/main.js
+++ exercises/ice-cream/scripts/main.js
@@ -20,8 +20,23 @@ function init() {
 function submitOrder() {
   //get the html input element into a variable, the id needs to match the id attribute in the html
   const numberOfScoopsInput = document.getElementById("numberOfScoopsInput");
-  let numberOfScoops = numberOfScoopsInput.value;
-  console.log(numberOfScoops);
+  const basePriceTD = document.getElementById("basePriceTD");
+  const totalDueTD = document.getElementById("totalDueTD");
+  //the basePriceTD holds an object like the one shown below which represents this html:
+  //<td id="basePriceTD">xxx.xx</td>
+  //   const basePriceTD = {
+  //     innerText: "xxx.xx",
+  //   };
+
+  //create variable numberOfScoops
+  //assign into the variable the value that was entered by the user
+  //otherwise known as the value property of the HTMLInputElement
+  //calling the Number function which takes a string as a parameter and returns the string converted into a number
+  let numberOfScoops = Number(numberOfScoopsInput.value);
+  const scoopCost = 2.25;
+  let basePrice = numberOfScoops * scoopCost;
+  basePriceTD.innerText = "$" + basePrice.toFixed(2);
+  totalDueTD.innerText = "$" + basePrice.toFixed(2);
 }
 
 //run this function after the window has loaded
